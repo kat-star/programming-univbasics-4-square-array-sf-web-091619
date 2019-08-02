c1890fe6db6c433134c25ce37177b13c6ef16008
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-02 00:14:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,20 +1,20 @@
-# def square_array(array)
-#   i = 0
-#   squared_arr = []
-  
-#   while i < array.length do
-#     squared_arr << array[i] ** 2
-#     i += 1
-#   end
-#   squared_arr
-# end
-
 def square_array(array)
   i = 0
   squared_arr = []
   
-  array.each do |num|
-    squared_arr << num ** 2
+  while i < array.length do
+    squared_arr << array[i] ** 2
+    i += 1
   end
   squared_arr
 end
+
+# def square_array(array)
+#   i = 0
+#   squared_arr = []
+  
+#   array.each do |num|
+#     squared_arr << num ** 2
+#   end
+#   squared_arr
+# end
